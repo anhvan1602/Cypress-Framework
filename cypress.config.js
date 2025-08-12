3ae8bc891382ff386134aecae2587e1d06bee052
anhvan1602 <vanhe1602@gmail.com>
2025-08-12 14:36:32 +0700

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -38,7 +38,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
 
   //Cypress uses your projectId and Record Key together to uniquely identify projects.
-  //projectId: '',
+  projectId: '96cso2',
 
   trashAssetsBeforeRuns : true,   //Whether Cypress will trash assets within the downloadsFolder, screenshotsFolder, and videosFolder before tests run with cypress run.
   
